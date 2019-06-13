c0522d66e9b67727efe9b0384d523f7b1d4a3e80
javen <wenxue@zbj.com>
2019-06-13 18:11:44 +0800

    udpate

--- src/main/java/com/track/toy/test/TestApplicationContext.java
+++ src/main/java/com/track/toy/test/TestApplicationContext.java
@@ -1,4 +1,18 @@
 package com.track.toy.test;
 
+import com.track.toy.graph.Graph;
+import com.track.toy.test.core.node.TestNode;
+
 public class TestApplicationContext {
+    public static void main(String[] args) {
+        //TODO 读取配置
+
+        //TODO 生成测试图
+        Graph<TestNode, Double, String, String> graph = null;
+
+        //TODO 给测试图灌入数据
+
+        //TODO 分批执行
+
+    }
 }
